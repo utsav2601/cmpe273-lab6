083785cbc2ac6dab4e736f510aedfe9ffbed7b05
Utsav Popli <utsav_popli@hotmail.com>
2014-11-10 16:57:21 -0800

    updated

--- client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
+++ client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
@@ -1,19 +1,31 @@
 package edu.sjsu.cmpe.cache.client;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.hash.Hashing;
+
 public class Client {
 
     public static void main(String[] args) throws Exception {
         System.out.println("Starting Cache Client...");
-        CacheServiceInterface cache = new DistributedCacheService(
-                "http://localhost:3000");
-
-        cache.put(1, "foo");
-        System.out.println("put(1 => foo)");
-
-        String value = cache.get(1);
-        System.out.println("get(1) => " + value);
-
-        System.out.println("Existing Cache Client...");
+        char[] value = {'0','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+        List<DistributedCacheService> CS = new ArrayList<DistributedCacheService>();
+        CS.add(new DistributedCacheService("http://localhost:3000"));
+        CS.add(new DistributedCacheService("http://localhost:3001"));
+        CS.add(new DistributedCacheService("http://localhost:3002"));
+        
+        for(int putkey=1; putkey<=10; putkey++)	{
+        	int key = Hashing.consistentHash(Hashing.md5().hashString(Integer.toString(putkey)), CS.size());
+        	CS.get(key).put(putkey, Character.toString(value[putkey]));
+        	System.out.println("The key value pair " + putkey +"-" + value[putkey]+ " is assigned to server " + key);
+        }
+        for(int getkey=1; getkey<=10; getkey++)	{
+        	int key2 = Hashing.consistentHash(Hashing.md5().hashString(Integer.toString(getkey)), CS.size());
+        	System.out.println("The key value pair " + getkey +"-" + CS.get(key2).get(getkey)+ " is received to server " + key2);
+        	
+        }
+        
     }
 
 }
